87cc576167396bcf3a969d2dc5dcdf919265e782
elkazo <kazoxyto@gmail.com>
2022-01-26 17:03:47 +0700

    added multi user

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
